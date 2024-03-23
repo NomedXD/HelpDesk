c8163e97db24eb99799c58de12b3e1f98fa4c625
Vlad <karelinvlad.dev@gmail.com>
2024-03-23 14:51:00 +0300

    add id in TicketResponse

--- src/main/java/com/innowise/dto/response/TicketResponse.java
+++ src/main/java/com/innowise/dto/response/TicketResponse.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public record TicketResponse(
+        Integer id,
         LocalDate createdOn,
         TicketState state,
         TicketUrgency urgency,
